85604d4ce4af9d40f13b36780bac195ccde4ad44
Adilton <adiltons28@gmail.com>
2024-01-22 12:48:48 -0300

    navbar bug fix

--- src/components/projs/Projects.jsx
+++ src/components/projs/Projects.jsx
@@ -1,5 +1,6 @@
 import styles from './Projects.module.scss'
 import coffeStackpreview from "../../assets/images/coffe stack.jpeg"
+import apexfitPreview from "../../assets/images/apexfit.png"
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
@@ -43,9 +44,14 @@ export default function Projects(){
                         <p>Uma landing page feita para um café, feita com tecnologias específicas para tornar a página atrativa e fornecer uma fantástica experiência.</p>
                     </div>
                     <div className={styles.item}>
-                        <img/>
-                        <h3>Em breve...</h3>
-                        <p></p>
+                    <div className={styles.imgCont}>
+                            <img src={apexfitPreview} alt="" />
+                            <a href="https://apexfit.vercel.app/" target='blank'></a>
+                        </div>
+
+                        <h3>
+                            <a href="https://apexfit.vercel.app/" target='blank'>Apex Fit</a></h3>
+                        <p>Uma landing page feita para uma academia, construida com tecnologias especificas para tornar a experiência de usuário única em qualquer tipo de dispositivo!</p>
                     </div>
                     <div className={styles.item}>
                         <img/>
